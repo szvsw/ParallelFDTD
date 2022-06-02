fb79388ce5afa3aa0b1c0199e7fb8ffb3b322c0c
Rantaharju Jarno <rantahj1@login3.triton.aalto.fi>
2022-06-02 13:54:56 +0300

    update CUDA flags

--- python/triton_batch.sh
+++ python/triton_batch.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -l
 #SBATCH -J parallelFDTD
 #SBATCH -o parallelFDTD.out
-#SBATCH -t 10:00:00
+#SBATCH -t 00:10:00
 #SBATCH -p gpu
-#SBATCH --gres=gpu:teslak80:8
+##SBATCH --gres=gpu:teslak80:8
+#SBATCH --constraint="volta|ampere|pascal"
+#SBATCH --gres=gpu:4
 #SBATCH --mem-per-cpu=128000
 
 mkdir -p return
 
+module load anaconda gcc/6.5.0 cuda/10.2.89 matlab/r2019b
+source activate PFDTD
 srun python testBench.py
 
 mv ./*.log ./return/
